6a29fd0074bcfa6cbc9423c445a55721424ad497
Shockwave <henrikmller8@gmail.com>
2024-09-19 11:15:56 +0200

    deleted getAllGamesModels

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -1,16 +1,5 @@
-import allGamesModel from "../models/allgamesmodel.js";
 import TeamModel from "../models/TeamModel.js";
 import GameModel from "../models/gameModel.js";
-
-export const getallGamesModel = async (req, res) => {
-  try {
-    const gameIds = await allGamesModel.find();
-
-    res.status(200).json(gameIds);
-  } catch (error) {
-    res.status(404).json({ message: error.message });
-  }
-};
 
 export const getTeamModel = async (req, res) => {
   try {
--- server/routes/client.js
+++ server/routes/client.js
@@ -1,12 +1,8 @@
 import express from "express";
-import {
-  getallGamesModel,
-  getTeamModel,
-  getGamesWithDetails,
-} from "../controllers/client.js";
+import { getTeamModel, getGamesWithDetails } from "../controllers/client.js";
 const router = express.Router();
 
-router.get("/allgamesmodels", getallGamesModel);
+//router.get("/allgamesmodels", getallGamesModel);
 router.get("/teammodels", getTeamModel);
 router.get("/gamemodels", getGamesWithDetails);
 
